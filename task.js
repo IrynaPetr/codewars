01709dec52e58e8d8d05e9e739027a8ef4718a03
IrynaPetr <iryna.bieliakova@gmail.com>
2023-04-07 23:41:44 +0300

    add

--- task.js
+++ task.js
@@ -49,3 +49,57 @@ function abbrevName(name){
   
 
 }
+
+function abbrevName2(name){
+
+  var nameArray = name.split(" ");
+  return (nameArray[0][0] + "." + nameArray[1][0]).toUpperCase();
+}
+
+function abbrevName3(name){
+
+  return name.split(' ').map(i => i[0].toUpperCase()).join('.')
+
+}
+
+function abbrevName4(name){
+  return name.split(' ').map(x => x.substr(0, 1).toUpperCase()).join('.');
+}
+
+// Write a function that takes an array of numbers and returns the sum 
+// of the numbers. The numbers can be negative or non-integer. If 
+// the array does not contain any numbers then you should return 0.
+
+function sum (numbers) {
+  const totalSumm = numbers.reduce((total, number) => {
+    return total + number;
+  }, 0);
+  
+  return totalSumm
+};
+
+let array = [1, 5.2, 4, 0, -1];
+
+function sum2 (array) {
+  let res = 0;
+  for (let i = 0; i < array.length; i++) {
+    res = res + array[i];
+  }
+  return res;
+  
+}
+console.log(sum(array));
+
+function sum3 (numbers) {
+  if(numbers.length === 0){
+    return 0;
+  }
+  else {
+    let sumNumbers = 0;
+    for (e of numbers) {
+      sumNumbers += e;
+    }
+    return sumNumbers;
+  }    
+};
+
